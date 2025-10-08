87f7f1b49f616a6615e22cdb07bb99e58f538fc3
BtoMJ <bto.mena.17@gmail.com>
2025-10-08 00:25:53 -0600

    Responsive para tablet y laptop portrait

--- src/components/Whats/Whats.jsx
+++ src/components/Whats/Whats.jsx
@@ -2,9 +2,7 @@ import { IoLogoWhatsapp } from "react-icons/io";
 import "./Whats.css";
 
 function Whats() {
-  //   const emojiCode = 0x1f90d;
-  //   const emoji = String.fromCodePoint(emojiCode);
-  const message = `¡Hola! 🤍 Quiero confirmar mi asistencia a la Boda de Juliana & Mauricio 👰🏾🤵🏾`;
+  const message = `¡Hola! 🤍 Quiero confirmar mi asistencia a la Boda de Juliana & Mauricio 👰🏻🤵🏻`;
   const number = "524761037311"; // sin signos ni espacios
   const waUrl = `https://wa.me/${number}?text=${encodeURIComponent(message)}`;
 
@@ -18,8 +16,7 @@ function Whats() {
         asistencia.
       </p>
       <a
-        href="https://wa.me/524761037311?text=%C2%A1Hola!%20%F0%9F%A4%8D%20Quiero%20confirmar%20mi%20asistencia%20a%20la%20Boda%20de%20Juliana%20%26%20Mauricio%20%F0%9F%91%B0%F0%9F%8F%BE%F0%9F%A4%B5%F0%9F%8F%BE"
-        // href="https://wa.me/524761037311?text=%C2%A1Hola!%20%F0%9F%98%83%20Quiero%20confirmar%20mi%20asistencia%20a%20la%20Boda%20de%20Juliana%20%26%20Mauricio%20%F0%9F%91%B0%F0%9F%A4%B5"
+        href="https://wa.me/524761037311?text=%C2%A1Hola!%20%F0%9F%A4%8D%20Quiero%20confirmar%20mi%20asistencia%20a%20la%20Boda%20de%20Juliana%20%26%20Mauricio%20%F0%9F%91%B0%F0%9F%8F%BB%F0%9F%A4%B5%F0%9F%8F%B"
         target="_blank"
         rel="noopener"
         aria-label="Confirmar Asistencia por WhatsApp"
